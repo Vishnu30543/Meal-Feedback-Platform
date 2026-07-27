f80d4158702ee4762d5aecc2f3f603b5c76e71e9
Y Vishnu Sai Vardhan <2200030543@kluniversity.in>
2026-07-27 17:38:43 +0530

    Testing Render Autodeploy

--- backend/src/main/java/com/ashram/feedback/resident/service/ResidentService.java
+++ backend/src/main/java/com/ashram/feedback/resident/service/ResidentService.java
@@ -21,6 +21,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
+
 import com.ashram.feedback.common.dto.PagedResponse;
 import com.ashram.feedback.common.exception.BusinessException;
 import com.ashram.feedback.common.exception.DuplicateResourceException;
